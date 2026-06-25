ba62069b9b0060a0a460fa5ae3850af6a3e8ea73
BorpBorp (Filoselle) <47865495+BorpBorp@users.noreply.github.com>
2026-06-25 14:00:23 -0400

    Corrections, new BlockBoundingBox Library

--- src/main/java/LuaCraft/LuaStom/sandbox/instance/UnitModifierLib.java
+++ src/main/java/LuaCraft/LuaStom/sandbox/instance/UnitModifierLib.java
@@ -6,6 +6,7 @@ import org.luaj.vm2.lib.ThreeArgFunction;
 
 import LuaCraft.LuaStom.LuaErrorAssert;
 import LuaCraft.LuaStom.sandbox.position.PointLib;
+import net.minestom.server.coordinate.Point;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.instance.generator.UnitModifier;
 
@@ -19,7 +20,9 @@ public class UnitModifierLib extends LuaTable {
             @Override
             public LuaValue call(LuaValue self, LuaValue minMaxHeight, LuaValue block) {
 
-                modifier.fillHeight(LuaErrorAssert.checkInt(minMaxHeight.get(1), "SetFillHeight", 1), LuaErrorAssert.checkInt(minMaxHeight.get(2), "SetFillHeight", 2), Block.fromKey(LuaErrorAssert.checkString(block, "SetFillHeight", 3)));
+                modifier.fillHeight(LuaErrorAssert.checkInt(minMaxHeight.get(1), "SetFillHeight", 1),
+                        LuaErrorAssert.checkInt(minMaxHeight.get(2), "SetFillHeight", 2),
+                        Block.fromKey(LuaErrorAssert.checkString(block, "SetFillHeight", 3)));
 
                 return LuaValue.NIL;
             }
@@ -28,8 +31,23 @@ public class UnitModifierLib extends LuaTable {
         rawset("SetFill", new ThreeArgFunction() {
             @Override
             public LuaValue call(LuaValue self, LuaValue fromTo, LuaValue block) {
-                modifier.fill(((PointLib) fromTo.get(1)).getPoint(), ((PointLib) fromTo.get(2)).getPoint(), Block.fromKey(LuaErrorAssert.checkString(block, "SetFill", 3)));
-            
+                modifier.fill(((PointLib) fromTo.get(1)).getPoint(), ((PointLib) fromTo.get(2)).getPoint(),
+                        Block.fromKey(LuaErrorAssert.checkString(block, "SetFill", 3)));
+
+                return LuaValue.NIL;
+            }
+        });
+
+        rawset("SetBlock", new ThreeArgFunction() {
+            @Override
+            public LuaValue call(LuaValue self, LuaValue pos, LuaValue bloc) {
+                if (pos instanceof PointLib pointLib) {
+                    Point point = pointLib.getPoint();
+                    Block block = Block.fromKey(LuaErrorAssert.checkString(bloc, "SetBlock", 2));
+
+                    modifier.setBlock(point, block);
+                }
+
                 return LuaValue.NIL;
             }
         });
